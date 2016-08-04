ee502cc6adf222a2e3a6330ff0e6086b03effe2f
Antonio Lopes <aclopesjr@hotmail.com>
2016-08-04 13:13:35 -0300

    Swift

---
+++ Carros Swift/Carros Swift/Controller/ListaCarrosViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ListaCarrosViewController.swift
+//  Carros Swift
+//
+//  Created by Antonio Lopes on 8/4/16.
+//  Copyright © 2016 Antonio Lopes. All rights reserved.
+//
+
+import UIKit
+
+class ListaCarrosViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.title = "Carros"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
