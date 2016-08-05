c6fa4a32dcc81612246185a427be5ce672fd95a2
Antonio Lopes <aclopesjr@hotmail.com>
2016-08-05 11:56:53 -0300

    Objective-C - Detalhes do Carro.

---
+++ Carros Objective-C/Carros Objective-C/Controller/DetalhesCarroViewController.m
@@ -0,0 +1,37 @@
+//
+//  DetalhesCarroViewController.m
+//  Carros Objective-C
+//
+//  Created by Antonio Lopes on 8/5/16.
+//  Copyright © 2016 Antonio Lopes. All rights reserved.
+//
+
+#import "DetalhesCarroViewController.h"
+
+@interface DetalhesCarroViewController ()
+
+@end
+
+@implementation DetalhesCarroViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
