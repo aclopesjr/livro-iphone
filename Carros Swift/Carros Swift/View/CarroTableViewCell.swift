865fe7aa8e5bf2fd2dedca169a9f3d4e2f9f76fa
Antonio Lopes <aclopesjr@hotmail.com>
2016-08-05 14:40:39 -0300

    Swift - TableViewCell

---
+++ Carros Swift/Carros Swift/View/CarroTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CarroTableViewCell.swift
+//  Carros Swift
+//
+//  Created by Antonio Lopes on 8/5/16.
+//  Copyright © 2016 Antonio Lopes. All rights reserved.
+//
+
+import UIKit
+
+class CarroTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
