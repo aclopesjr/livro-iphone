fe38a02f9c7007e9b4b157a5e3bc6671d3135873
Antonio Lopes <aclopesjr@hotmail.com>
2016-08-05 15:20:08 -0300

    Objective-C - TableViewCell.

---
+++ Carros Objective-C/Carros Objective-C/View/CarroTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  CarroTableViewCell.m
+//  Carros Objective-C
+//
+//  Created by Antonio Lopes on 8/5/16.
+//  Copyright © 2016 Antonio Lopes. All rights reserved.
+//
+
+#import "CarroTableViewCell.h"
+
+@implementation CarroTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
