64666284b77c766a868964040265a1f09c95e530
Antonio Lopes <aclopesjr@hotmail.com>
2016-10-18 16:25:10 -0200

    Switft 3.0 - Lendo as coordenadas GPS com o framework Core Locaiton

---
+++ Carros Swift/Carros Swift/Controller/GpsMapViewController.swift
@@ -0,0 +1,35 @@
+//
+//  GpsMapViewController.swift
+//  Carros Swift
+//
+//  Created by Antonio Lopes on 10/18/16.
+//  Copyright © 2016 Antonio Lopes. All rights reserved.
+//
+
+import UIKit
+
+class GpsMapViewController: MapViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
